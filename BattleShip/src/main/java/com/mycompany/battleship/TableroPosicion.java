fecf86500d062f8f01a8a1e850dbe4aa7fb9c0c9
Francisco Javier Tropa Huircan <89926929+FrancisTropa@users.noreply.github.com>
2022-07-05 02:39:46 -0400

    Add files via upload

---
+++ BattleShip/src/main/java/com/mycompany/battleship/TableroPosicion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.battleship;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author L1kir
+ */
+public class TableroPosicion extends JPanel{
+    
+    public void setBackground() {
+        super.setBackground(new java.awt.Color(0, 69, 167));
+    }
+
+    public void setForeground() {
+        super.setForeground(new java.awt.Color(255, 255, 255));
+    }
+
+    public void setPreferredSize() {
+        super.setPreferredSize(new java.awt.Dimension(470, 470));
+    }
+    
+}
